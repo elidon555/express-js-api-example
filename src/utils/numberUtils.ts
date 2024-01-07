cb19bd9521bcb833bc4919ce771d9070e2f75fb3
elidon555 <elidonneziri@gmail.com>
2024-01-07 16:22:48 +0100

    algorithm-improvements

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -1,11 +1,20 @@
 export function isPalindrome(num: number): boolean {
+    if (num < 0) return false;
+
     const str = num.toString();
-    return str === str.split('').reverse().join('');
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 export function isPrime(number: number): boolean {
     if (number <= 1) return false;
-    for (let i = 2; i * i <= number; i++) {
+    if (number === 2) return true;
+    if (number % 2 === 0) return false;
+    for (let i = 3; i * i <= number; i += 2) {
         if (number % i === 0) return false;
     }
     return true;
